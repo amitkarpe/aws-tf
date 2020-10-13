5054e1a9f30279e96a21f06d2cbb007c127c4ba1
Amit Karpe <amitkarpe@gmail.com>
2020-10-13 17:45:02 +0800

    demo2 and demo3 are not working

--- ec2/demo/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "public_key_path" {
-	default="~/.ssh/id_rsa.pub"
-}
--- ec2/demo3/demo.tf
+++ ec2/demo3/demo.tf
@@ -13,6 +13,10 @@ resource "aws_instance" "example" {
   ami           = "ami-0947d2ba12ee1ff75"
   instance_type = "t3.medium"
 
+  tags = {
+    Name = "demo3"
+  }
+
   connection {
     type        = "ssh"
     user        = "ec2-user"
