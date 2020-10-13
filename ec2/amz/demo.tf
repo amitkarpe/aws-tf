760b287af83f45e3cd616991c1716b88c5a2f819
Amit Karpe <amitkarpe@gmail.com>
2020-10-13 20:01:15 +0800

    Confirmed that ubuntu and amazon are working using ssh-key

--- ec2/amz/demo.tf
+++ ec2/amz/demo.tf
@@ -1,17 +1,17 @@
 provider "aws" {
   profile = "default"
   region  = "us-east-1"
-  }
+}
 
 resource "aws_key_pair" "example" {
   key_name   = "examplekey3"
-  public_key = file("~/.ssh/terraform.pub")
+  public_key = file("terraform.pub")
 }
 
 resource "aws_instance" "example" {
   key_name      = aws_key_pair.example.key_name
   ami           = "ami-0947d2ba12ee1ff75"
-  instance_type = "t3.medium"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "demo3"
@@ -20,8 +20,9 @@ resource "aws_instance" "example" {
   connection {
     type        = "ssh"
     user        = "ec2-user"
-    private_key = file("~/.ssh/terraform")
-    host        = self.public_ip
+    private_key = file("terraform")
+    #    host        = self.public_ip
+    host = coalesce(self.public_ip, self.private_ip)
   }
 
   provisioner "remote-exec" {
@@ -35,8 +36,6 @@ resource "aws_instance" "example" {
 
 output dns {
   value       = aws_instance.example.public_dns
-  sensitive   = true
   description = "export host=$(tf show | grep -i public_dns | awk {'print $3'} | sed 's/\"//g'); echo $host; ssh ec2-user@$host"
-  depends_on  = []
 }
 
